ca8b8cd7e1a3f678f29dfb2fc6414c53ac190d58
Laia_Zapata <20220448@itla.edu.do>
2023-09-29 19:39:27 -0400

    Agregar archivos de proyecto.

---
+++ Application/ViewModel/conversionViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModel
+{
+    public class conversionViewModel
+    {
+        public double Amount { get; set; }
+        public string FromCurrency { get; set; }
+        public string ToCurrency { get; set; }
+
+        public double Result
+        {
+            get
+            {
+                
+                double pesoToDollarRate = 0.018; 
+                double pesoToEuroRate = 0.015;   
+                double euroToDollarRate = 1.20; 
+
+                double result = 0;
+
+                if (FromCurrency == "peso" && ToCurrency == "dolar")
+                {
+                    result = Amount * pesoToDollarRate;
+                }
+                else if (FromCurrency == "dolar" && ToCurrency == "peso")
+                {
+                    result = Amount / pesoToDollarRate;
+                }
+                else if (FromCurrency == "peso" && ToCurrency == "euro")
+                {
+                    result = Amount * pesoToEuroRate;
+                }
+                else if (FromCurrency == "euro" && ToCurrency == "peso")
+                {
+                    result = Amount / pesoToEuroRate;
+                }
+                else if (FromCurrency == "euro" && ToCurrency == "dolar")
+                {
+                    result = Amount * euroToDollarRate;
+                }
+                else if (FromCurrency == "dolar" && ToCurrency == "euro")
+                {
+                    result = Amount / euroToDollarRate;
+                }
+                else if (FromCurrency == "peso" && ToCurrency == "peso")
+                {
+                    result = Amount;
+                }
+                else if (FromCurrency == "euro" && ToCurrency == "euro")
+                {
+                    result = Amount;
+                }
+                else if (FromCurrency == "dolar" && ToCurrency == "dolar")
+                {
+                    result = Amount;
+                }
+
+                return result;
+            }
+        }
+    }
+
+   
+}
---
+++ ConversionDeMonedas/Controllers/conversionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.ViewModel;
+
+namespace ConversionDeMonedas.Controllers
+{
+    public class conversionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ConvertCurrency()
+        {
+            var model = new conversionViewModel();
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult ConvertCurrency(conversionViewModel model)
+        {
+            return View("Index", model);
+        }
+
+    }
+}
